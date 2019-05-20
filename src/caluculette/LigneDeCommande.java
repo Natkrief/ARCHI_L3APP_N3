b7fa4666e148c7f126c1f4a150149a7c82658d45
Nathaniel Krief <nathan55@hotmail.fr>
2019-05-20 11:53:46 +0200

    UnitDivision

--- src/caluculette/LigneDeCommande.java
+++ src/caluculette/LigneDeCommande.java
@@ -1,6 +1,7 @@
 package caluculette;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -13,31 +14,43 @@ public class LigneDeCommande implements IHM {
 
 	@Override
 	public void demarer() {
-
 		try {
-			System.out.println("Choisissez une première valeur");
-			double res = sc.nextDouble();
-
-			System.out.println("Choisissez une deuxième valeur");
-			double res2 = sc.nextDouble();
-
+			double res = valeur();
+			double res2 = valeur();
 			char choix = afficheMenu();
 			double resultat = Calculator.calculer(res, res2, choix);
 			System.out.println(res + " " + choix + " " + res2 + " = " + resultat);
-		} catch (MonException e ) {
+		}
+
+		catch (MonException e) {
 			System.out.println(e.getMessage());
-			
-		} 
-		
-		
+
+		}
 
 	}
 
-	public char afficheMenu() {
-		System.out.println("Taper '+' pour additionner\nTapez '/' pour Diviser\nTapez 'q' pour Quitter");
-		String choix = sc.next();
-		char res = (char) choix.charAt(0);
-		return res;
+	public char afficheMenu() throws MonException{
+		char res;
+		try {
+			System.out.println("Taper '+' pour additionner\nTapez '/' pour Diviser");
+			String choix = sc.next();
+			res = (char) choix.charAt(0);
+			return res;
+		} catch(Exception e) {
+			throw new MonException(EnumException.CHAR_INVALIDE.getCode(),EnumException.CHAR_INVALIDE.getMessage());
+		}
+
+	}
+
+	public double valeur() throws MonException {
+		double res = 0;
+		try {
+			System.out.println("Choisissez une valeur");
+			res = sc.nextDouble();
+			return res;
+		} catch (InputMismatchException e) {
+			throw new MonException(EnumException.VALEUR_INVALIDE.getCode(),EnumException.VALEUR_INVALIDE.getMessage());
+		}
 	}
 
 }
--- src/testUnit/testDivision.java
+++ src/testUnit/testDivision.java
@@ -2,9 +2,16 @@ package testUnit;
 
 import static org.junit.Assert.*;
 
+import java.rmi.server.Operation;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import caluculette.Calculator;
+import caluculette.Division;
+import caluculette.EnumException;
+import caluculette.MonException;
 
 public class testDivision {
 
@@ -16,9 +23,20 @@ public class testDivision {
 	public void tearDown() throws Exception {
 	}
 
+	@Test(expected=MonException.class)
+	public void testDivisionParZero() throws MonException {
+		double a = 15;
+		double b = 0;
+		Division div = new Division();
+		assertEquals(div.calculer(a, b),Calculator.calculer(a, b, '/'));
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDivisionValide() throws MonException {
+		double a = 15;
+		double b = 3;
+		Division div = new Division();
+		assertEquals(5.0,div.calculer(a, b),0);
 	}
 
 }
